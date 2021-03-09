3be35de6c7a2e608434521a5204b87dff577cb4d
Fjodor <fjordormaster@hotmail.com>
2021-03-09 09:06:48 +0100

    move around path part

--- install_jetson.sh
+++ install_jetson.sh
@@ -168,6 +168,12 @@ sed -e '/MinkowskiEngine/ s/^#*/#/' -i requirements.txt
 
 python3.7 -m pip install -r requirements.txt
 
+# export paths
+echo "export PYTHONPATH=$PYTHONPATH:$HOME/repos/FCGF" >> $HOME/.bashrc
+echo "export PYTHONPATH=$PYTHONPATH:$HOME/repos/DeepGlobalRegistration" >> $HOME/.bashrc
+echo "export PYTHONPATH=$PYTHONPATH:$HOME/repos/TEASER-plusplus/examples/teaser_python_fpfh_icp" >> $HOME/.bashrc
+
+
 # INSTALL ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
@@ -186,9 +192,6 @@ sudo apt install -y ros-melodic-pcl-ros python-catkin-tools ros-melodic-tf2-sens
 cd $HOME/repos/inspectrone/catkin_ws/
 catkin build -DPYTHON_EXECUTABLE=$(which python3.7) # on first build
 
-echo "export PYTHONPATH=$PYTHONPATH:$HOME/repos/FCGF" >> $HOME/.bashrc
-echo "export PYTHONPATH=$PYTHONPATH:$HOME/repos/DeepGlobalRegistration" >> $HOME/.bashrc
-echo "export PYTHONPATH=$PYTHONPATH:$HOME/repos/TEASER-plusplus/examples/teaser_python_fpfh_icp" >> $HOME/.bashrc
 
 
 
