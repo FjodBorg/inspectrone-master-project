1c3de8a04cadf0393a8b76b3fd6f4d5cf4e40106
Fjodor <Fjordormaster@hotmail.com>
2021-03-31 10:27:20 +0200

    add more dependencies

--- install_jetson.sh
+++ install_jetson.sh
@@ -217,7 +217,7 @@ source /opt/ros/melodic/setup.bash
 
 # more dependencies
 sudo apt install -y ros-melodic-pcl-ros ros-melodic-tf2-sensor-msgs rviz ros-melodic-ros-numpy
-
+python3.7 -m pip install pycryptodome pycryptodomex
 
 #sudo apt install -y python-catkin-tools 
 #sudo apt install -y python3-catkin-pkg-modules python3-rospkg-modules python3-empy
