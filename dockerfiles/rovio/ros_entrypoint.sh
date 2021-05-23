c2e87db99df036462b225e8e5e8de7358fde93ea
Fjodor <Fjordormaster@hotmail.com>
2021-05-23 14:38:28 +0200

    change of structure

--- dockerfiles/rovio/ros_entrypoint.sh
+++ dockerfiles/rovio/ros_entrypoint.sh
@@ -1,6 +1,36 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
-
+# bash
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
+source $HOME/.bashrc
+
+
+# echo "${cmd_array%...}"
+# echo "catkin build rovio_extras" >> $HOME/.bashrc 
+# catkin build rovio_extras 
+# echo "yee"
+
+find $HOME/catkin_ws/src/rovio_extras/launch/ -maxdepth 1 -mindepth 1 -type f -exec ln -s '{}' $HOME/catkin_ws/src/rovio/launch/ \;
+
+cmd_array=$EXTRA_OPTIONS
+
+success=true
+IFS='...' read -ra ADDR <<< "$cmd_array"
+for i in "${ADDR[@]}"; do
+  if [ ! -z "$i" ]; then # check if not empty string
+    echo "Running command: $i"
+    eval $i || echo "    please check your additional argument passed with start_container.sh" && success=false
+  fi
+done
+ 
+if ! $success ; then
+  exit
+fi 
+
+
+
+source $HOME/.bashrc
+
 exec "$@"
+
